d3e4ffc2ad7a5faa1b2b3f773ff16e59b9a831bf
toyobayashi <lifenglin314@outlook.com>
2024-01-24 23:22:28 +0800

    use packed emnapi

--- scripts/patch.js
+++ scripts/patch.js
@@ -1,11 +1,6 @@
 const { join } = require('path')
 const { spawnSync } = require('child_process')
 
-spawnSync('patch', ['-p3', '-i', join(__dirname, '../patches/emnapi.patch')], {
-  cwd: join(__dirname, '../node_modules/emnapi'),
-  stdio: 'inherit'
-})
-
 spawnSync('patch', ['-p1', '-i', join(__dirname, '../patches/node-gyp.patch')], {
   cwd: join(__dirname, '../node_modules/node-gyp/gyp'),
   stdio: 'inherit'
